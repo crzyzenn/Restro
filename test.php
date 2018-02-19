6798cef863c652244006caff0acab4fd044c8cf1
Zenish Shrestha <zenishshr@gmail.com>
2018-02-19 17:47:49 +0545

    Added a new home layout\nAdded responsive design\n

---
+++ test.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mobile test</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Restaurant</title>
+	<link rel = 'stylesheet' media="screen" href = 'bs/css/bootstrap.css'>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src = 'bs/js/bootstrap.js'></script>		
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#"><img src="Images/pizzahut-logo.png" class = 'home_img'></a>
+				</div>
+		
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<!-- <ul class="nav navbar-nav navbar-right">
+						<li><a href="#">Link</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Action</a></li>
+								<li><a href="#">Another action</a></li>
+								<li><a href="#">Something else here</a></li>
+								<li><a href="#">Separated link</a></li>
+							</ul>
+						</li>
+					</ul> -->
+					<ul class="nav navbar-nav navbar-right">
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="#">About Us</a></li>
+						<li><a href="#">Contact Us</a></li>
+					</ul>
+					<!-- <form class="navbar-form navbar-left" role="search">
+						<div class="form-group">
+							<input type="text" class="form-control" placeholder="Search">
+						</div>
+						<button type="submit" class="btn btn-default">Submit</button>
+					</form> -->
+					
+				</div><!-- /.navbar-collapse -->
+			</div>
+		</nav>		
+	</header>
+
+
+	<!-- Form table number -->
+	<div class="container-fluid">
+		<form action="" method="POST" role="form">
+			
+			<table class="table">
+			
+				<tbody>
+					<tr>
+						<td><br>Table Number</td>
+						<td>
+							<br>
+							<div class="form-group">			
+								<input type="name" class="form-control" id="" placeholder="Table Number">
+							</div>	
+						</td>
+					</tr>
+					<tr>
+						<td><br><button type="submit" class="btn btn-primary btn-sm"><span class = 'glyphicon glyphicon-ok-circle'></span> GO</button></td>
+					</tr>
+				</tbody>
+			</table>
+			
+		</form>
+	</div>
+
+	<!-- End of form -->
+	<hr>
+	<footer>
+	
+	</footer>
+</body>
+</html>
